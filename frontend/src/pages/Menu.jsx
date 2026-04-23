9952d9249f6159e8a560a4551a1bce7fc0957d4d
kevin-nguyen-pro <dangkhoa04052005@gmail.com>
2026-04-24 00:45:50 +0930

    Implement MenuLoader and get items into Menu page.

--- frontend/src/pages/Menu.jsx
+++ frontend/src/pages/Menu.jsx
@@ -1,4 +1,7 @@
-
+import { useState, useEffect } from "react";
+import { fetchRestaurantMenu } from "../api/Restaurant";
+import useRestaurant from "../hooks/useRestaurant"
+import MenuLoader from "../components/loader/MenuLoader";
 
 const menuItems = [
   { name: "Margherita Pizza", price: 12.99, category: "Main" },
@@ -9,14 +12,41 @@ const menuItems = [
 ];
 
 export default function Menu() {
+  const [items, setItems] = useState([])
+  const [restaurantId, setRestaurantId] = useState(1)
+  const {restaurants, loading, error} = useRestaurant();
+
+  
+  useEffect(() => {
+      fetchRestaurantMenu(restaurantId).then(data => setItems(data))
+  }, [restaurantId])
+
+  if (error) {
+        return (<div> It's error </div>)
+  }
+  if (loading) return <MenuLoader/>
+
+
   return (
-    <div className="p-6 w-3/4 mx-auto">
+    <div className="p-6 w-full mx-auto">
       <h2 className="font-semibold text-center mb-4 text-3xl font-manrope text-white/70 -mt-3">Menu Restaurants </h2>
 
-      <select className="mb-4 px-3 py-2 rounded-md bg-gray-800 text-white border border-yellow-500 focus:outline-none">
-        <option>Restaurant 1</option>
-        <option>Restaurant 2</option>
-      </select>
+      {loading ? (
+      <select
+        disabled
+        className="mb-4 px-3 py-2 bg-gray-100 border border-gray-200 
+        rounded-xl text-sm text-gray-400 appearance-none cursor-not-allowed opacity-60"
+        >
+          <option>Loading...</option>
+        </select>) : (
+          <select 
+            onChange={(e) => setRestaurantId(e.target.value)}
+            className="mb-4 px-3 py-2 rounded-md bg-gray-800 text-white border border-yellow-500 focus:outline-none">
+              {restaurants.map((res) => (
+                  <option key={res.id} value={res.id}>{res.name}</option>
+              ))}
+          </select>
+        )}
 
       <div className="overflow-x-auto rounded-lg border border-yellow-500">
         <table className="w-full text-sm text-left text-white">
@@ -30,7 +60,7 @@ export default function Menu() {
           </thead>
 
           <tbody>
-            {menuItems.map((item, i) => (
+            {items.map((item, i) => (
               <tr
                 key={i}
                 className={`border-b border-gray-700 hover:bg-gray-700 transition-colors ${
@@ -41,9 +71,9 @@ export default function Menu() {
                 <td className="px-6 py-3">${item.price.toFixed(2)}</td>
                 <td className="px-6 py-3">
                   <span className={`px-2 py-1 rounded-full text-xs font-medium ${
-                    item.category === "Main"    ? "bg-blue-900 text-blue-300" :
-                    item.category === "Starter" ? "bg-green-900 text-green-300" :
-                    item.category === "Dessert" ? "bg-pink-900 text-pink-300" :
+                    item.category === "food"    ? "bg-blue-900 text-blue-300" :
+                    item.category === "drink" ? "bg-green-900 text-green-300" :
+                    item.category === "dessert" ? "bg-pink-900 text-pink-300" :
                     "bg-gray-700 text-gray-300"
                   }`}>
                     {item.category}
