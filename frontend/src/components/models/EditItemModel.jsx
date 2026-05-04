e77604f83d1d10e8b24ed404383bc5ec4311527e
kevin-nguyen-pro <dangkhoa04052005@gmail.com>
2026-05-04 18:17:23 +0930

    Fix an error to deploy an website.

--- frontend/src/components/models/EditItemModel.jsx
+++ frontend/src/components/models/EditItemModel.jsx
@@ -16,6 +16,9 @@ export default function EditItemModel({ item, onClose, onSave }) {
         onSave(formData)
     }
 
+    if (categoryError) {
+      <div> It's error </div>
+    }
   return (
     <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
       <div className="bg-gray-900 border border-gray-700 rounded-xl p-6 w-96">
